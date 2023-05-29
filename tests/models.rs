04f2f4a35e833242a0c5846f11754ad7c549b704
Matteo Carnelos <matteo.carnelos@studenti.unipd.it>
2023-05-29 11:54:46 +0200

    test: add person_detect integration test

--- tests/models.rs
+++ tests/models.rs
@@ -20,3 +20,13 @@ fn speech_model() {
     let output = matrix![0.0234375, 0.23828125, 0.3359375, 0.40234375];
     assert_eq!(Speech::predict(input), output);
 }
+
+#[model("models/person_detect.tflite")]
+struct PersonDetect;
+
+#[test]
+fn person_detect_model() {
+    let input = [SMatrix::from_element([0.5])];
+    let output = matrix![0., 0.];
+    assert_eq!(PersonDetect::predict(input), output);
+}
